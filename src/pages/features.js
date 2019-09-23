d299ea6f1c296b70147010a3471cec07ff510a8a
Adel <htwr86@gmail.com>
2019-09-23 08:09:02 +0300

    add features page

--- src/pages/features.js
+++ src/pages/features.js
@@ -1,14 +1,83 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout.js"
+import "./features.scss"
 
 const Features = () => {
   return (
     <Layout>
-      <div>
-        <h1 className="title is-1">Features</h1>
-        <Link to="/">to home</Link>
-      </div>
+      <section className="hero f-hero is-fullheight is-dark">
+        <div className="hero-body">
+          <div className="columns">
+            <div class="column"></div>
+            <div class="column is-three-fifths i-column">
+              <div className="i-container has-text-centered">
+                <h1 className="title is-1 has-text-weight-bold">
+                  Commercial dump truck dispatch software saves you time and
+                  money!
+                </h1>
+
+                <button className="button is-medium i-button">
+                  REQUEST DEMO
+                </button>
+              </div>
+            </div>
+
+            <div class="column"></div>
+          </div>
+        </div>
+      </section>
+
+      <section className="hero is-info is-fullheight">
+        <div className="hero-body">
+          <div className="container">
+            <h2 className="title is-2 has-text-centered">
+              Create and manage projects and quotes
+            </h2>
+            <div className="columns">
+              <div className="column">
+                <content className="content">
+                  <p>
+                    Are you still using a paper based system to track orders and
+                    create quotes for customers? There is an easier and more
+                    productive way! Setting up a new project or getting your bid
+                    in for a contract is fast and easy with Dump Truck
+                    Dispatcher, a cloud based software program for dump truck
+                    businesses.
+                  </p>
+
+                  <p>
+                    Projects allow you to create numerous quotes associated with
+                    the same project without having to enter the same data for
+                    each quote you are sending to the various vendors. You can
+                    readily track the quotes associated with the project.
+                  </p>
+                  <p>
+                    Once data is entered into a quote, it can carry through to
+                    the orders with minimal data entry. This streamlines the
+                    process, reduces data entry, and results in fewer errors in
+                    pricing.
+                  </p>
+
+                  <p>
+                    You have the ability to sort and search for projects by
+                    start date, customer name, and status.
+                  </p>
+                </content>
+              </div>
+              <div className="column">
+                <figure>
+                  <img
+                    src="../images/projects-2-1024x768.jpg"
+                    alt="projects"
+                    width="300"
+                  />
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,6 @@ const indexPage = () => {
   return (
     <Layout>
       <section className="hero hero-index is-fullheight is-dark">
-        {/* <div className="hero-head i-hero-head is-light">
-          <div className="nav-bar"></div>
-        </div>
-        <div className="hero-body"> */}
-        {/* <div className="container"></div> */}
         <div className="columns">
           <div class="column"></div>
           <div class="column is-three-fifths i-column">
@@ -37,44 +32,47 @@ const indexPage = () => {
       </section>
 
       <section className="hero is-light is-fullheight">
-        {/* <div className="container"> */}
-        <div className="container">
-          <h2 className="title">Benefits</h2>
-          <div className="columns">
-            <div className="column ">
-              <div className="content is-medium">
-                <ul className="type-1">
-                  <li>Improved dispatcher productivity</li>
-                  <li>Happier dispatchers and customers</li>
-                  <li>Increased truck utilization</li>
-                  <li>Reduced labor expenses</li>
-                  <li>
-                    Reduced fleet vehicle maintenance cost and fewer unplanned
-                    breakdowns
-                  </li>
-                  <li>Increased profitability</li>
-                </ul>
+        <div className="hero-body">
+          <div className="container">
+            <h2 className="title">Benefits</h2>
+            <div className="columns">
+              <div className="column ">
+                <div className="content is-medium">
+                  <ul className="type-1">
+                    <li>
+                      <i class="fa fa-check" aria-hidden="true"></i>Improved
+                      dispatcher productivity
+                    </li>
+                    <li>Happier dispatchers and customers</li>
+                    <li>Increased truck utilization</li>
+                    <li>Reduced labor expenses</li>
+                    <li>
+                      Reduced fleet vehicle maintenance cost and fewer unplanned
+                      breakdowns
+                    </li>
+                    <li>Increased profitability</li>
+                  </ul>
+                </div>
               </div>
-            </div>
 
-            <div className="column ">
-              <div className="content is-medium">
-                <p>
-                  Some of these benefits may sound too good to be true. Read
-                  below to find out more about how Dump Truck Dispatcher can
-                  improve your operations and provide these benefits.
-                </p>
-                <p>
-                  Ask about our case study of a client with a large fleet. They
-                  had a 28% improvement in revenue per mile and 26% improvement
-                  in revenue per hour. And they haven't finished implementing
-                  all of the functionality.
-                </p>
+              <div className="column ">
+                <div className="content is-medium">
+                  <p>
+                    Some of these benefits may sound too good to be true. Read
+                    below to find out more about how Dump Truck Dispatcher can
+                    improve your operations and provide these benefits.
+                  </p>
+                  <p>
+                    Ask about our case study of a client with a large fleet.
+                    They had a 28% improvement in revenue per mile and 26%
+                    improvement in revenue per hour. And they haven't finished
+                    implementing all of the functionality.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        {/* </div> */}
       </section>
     </Layout>
   )
